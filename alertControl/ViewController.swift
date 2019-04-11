2d4fd595bbca415b3749ab8fe8d11f23e4420c74
D7703_15 <d7703_15@d7703-15ui-imac.local>
2019-04-11 11:41:49 +0900

    Initial Commit

---
+++ alertControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  alertControl
+//
+//  Created by D7703_15 on 2019. 4. 11..
+//  Copyright © 2019년 BlueScreen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
